e81ccd837d7c9a813a219a6bc683bca9828d4562
t-abagar <t-abagar@microsoft.com>
2016-07-26 22:19:56 +0530

    Android Project Files initial commit

---
+++ android-naradaoffline/Application/src/main/java/com/example/android/naradaoffline/DatagramSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.naradaoffline;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DatagramSelectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datagram_selection);
+    }
+
+}
